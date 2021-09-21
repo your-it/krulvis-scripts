1b68b686c129d2a4181a53120051847cd497255b
krulvis <git@krulvis.space>
2021-09-21 16:53:50 +0200

    Thiever 1.0.7 make left-click optional, walk down from accidentally going up

--- src/main/kotlin/org/powbot/krulvis/thiever/tree/branch/Branches.kt
+++ src/main/kotlin/org/powbot/krulvis/thiever/tree/branch/Branches.kt
@@ -1,13 +1,16 @@
 package org.powbot.krulvis.thiever.tree.branch
 
-import org.powbot.api.rt4.Bank
-import org.powbot.api.rt4.Inventory
+import org.powbot.api.rt4.*
 import org.powbot.krulvis.api.ATContext.currentHP
 import org.powbot.krulvis.api.extensions.BankLocation.Companion.openNearestBank
 import org.powbot.api.script.tree.Branch
 import org.powbot.api.script.tree.SimpleLeaf
 import org.powbot.api.script.tree.TreeComponent
 import org.powbot.krulvis.api.ATContext
+import org.powbot.krulvis.api.ATContext.distance
+import org.powbot.krulvis.api.ATContext.interact
+import org.powbot.krulvis.api.utils.Utils.long
+import org.powbot.krulvis.api.utils.Utils.waitFor
 import org.powbot.krulvis.thiever.Thiever
 import org.powbot.krulvis.thiever.tree.leaf.Eat
 import org.powbot.krulvis.thiever.tree.leaf.HandleBank
@@ -26,14 +29,14 @@ class ShouldEat(script: Thiever) : Branch<Thiever>(script, "Should Eat") {
 
 class ShouldBank(script: Thiever) : Branch<Thiever>(script, "Should Bank") {
     override val successComponent: TreeComponent<Thiever> = ShouldOpenCoinPouch(script, IsBankOpen(script), 1)
-    override val failedComponent: TreeComponent<Thiever> = Pickpocket(script)
+    override val failedComponent: TreeComponent<Thiever> = AtSpot(script)
 
     override fun validate(): Boolean {
         return Inventory.isFull() || (currentHP() < 8 && !script.food.inInventory())
     }
 }
 
-class ShouldOpenCoinPouch(script: Thiever, nextNode: TreeComponent<Thiever>, val stackSize: Int) :
+class ShouldOpenCoinPouch(script: Thiever, nextNode: TreeComponent<Thiever>, private val stackSize: Int) :
     Branch<Thiever>(script, "Should Bank") {
     override val successComponent: TreeComponent<Thiever> = OpenPouch(script)
     override val failedComponent: TreeComponent<Thiever> = nextNode
@@ -52,5 +55,24 @@ class IsBankOpen(script: Thiever) : Branch<Thiever>(script, "Should Open Bank") 
 
     override fun validate(): Boolean {
         return Bank.opened()
+    }
+}
+
+class AtSpot(script: Thiever) : Branch<Thiever>(script, "AtSpot?") {
+    override val successComponent: TreeComponent<Thiever> = Pickpocket(script)
+    override val failedComponent: TreeComponent<Thiever> = SimpleLeaf(script, "Walking") {
+        Bank.close()
+        if (Players.local().tile().floor > 0) {
+            script.log.info("Climbing down first...")
+            if (interact(Objects.stream().action("Climb-down").nearest().firstOrNull(), "Climb-down")) {
+                waitFor(long()) { validate() }
+            }
+        } else {
+            Movement.walkTo(script.lastTile)
+        }
+    }
+
+    override fun validate(): Boolean {
+        return (script.getTarget()?.distance() ?: 99) < 20
     }
 }
