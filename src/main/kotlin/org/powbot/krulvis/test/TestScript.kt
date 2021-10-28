d415d7e715686aa60cff5dd35f6041382540bebf
krulvis <git@krulvis.space>
2021-10-28 15:12:51 +0200

    Fix errors -> unused imports and canCast

--- src/main/kotlin/org/powbot/krulvis/test/TestScript.kt
+++ src/main/kotlin/org/powbot/krulvis/test/TestScript.kt
@@ -3,10 +3,7 @@ package org.powbot.krulvis.test
 import org.powbot.api.*
 import org.powbot.api.event.*
 import org.powbot.api.rt4.*
-import org.powbot.api.rt4.magic.Rune
-import org.powbot.api.rt4.magic.Staff
 import org.powbot.api.rt4.walking.local.LocalPath
-import org.powbot.api.rt4.walking.local.Utils
 import org.powbot.api.rt4.walking.model.Edge
 import org.powbot.api.script.OptionType
 import org.powbot.api.script.ScriptConfiguration
@@ -17,7 +14,6 @@ import org.powbot.api.script.tree.TreeComponent
 import org.powbot.krulvis.api.script.ATScript
 import org.powbot.krulvis.api.script.painter.ATPaint
 import org.powbot.krulvis.api.utils.Utils.sleep
-import org.powbot.krulvis.miner.Data
 import org.powbot.mobile.drawing.Graphics
 
 @ScriptManifest(name = "Krul TestScriptu", version = "1.0.1", description = "", priv = true)
@@ -98,7 +94,7 @@ class TestPainter(script: TestScript) : ATPaint<TestScript>(script) {
     override fun buildPaint(paintBuilder: PaintBuilder): Paint {
         return paintBuilder
 //            .addString("Top poly:") { "${Data.TOP_POLY.contains(Players.local().tile())}" }
-            .addString("Can cast:") { "${Magic.LunarSpell.HUMIDIFY.canCast()}" }
+//            .addString("Can cast:") { "${Magic.LunarSpell.HUMIDIFY.canCast()}" }
             .withTotalLoot(true)
             .build()
     }
