691503332114e2cb0c85a1a80baac00945f3cd05
constt <ijavaservices@gmail.com>
2022-08-31 20:40:21 +1200

    Update Kill.kt

--- src/main/kotlin/org/powbot/krulvis/tempoross/tree/leaf/Kill.kt
+++ src/main/kotlin/org/powbot/krulvis/tempoross/tree/leaf/Kill.kt
@@ -8,14 +8,14 @@ import org.powbot.krulvis.api.utils.Utils.long
 import org.powbot.krulvis.api.utils.Utils.waitFor
 import org.powbot.krulvis.tempoross.Data.KILLING_ANIM
 import org.powbot.krulvis.tempoross.Tempoross
-
+import kotlin.math.roundToInt
 
 class Kill(script: Tempoross) : Leaf<Tempoross>(script, "Killing") {
 
     override fun execute() {
         script.forcedShooting = false
         val spirit = script.getBossPool()
-        val killing = me.animation() != -1 && (spirit?.distance() ?: 4) <= 3
+        val killing = me.animation() != -1 && (spirit?.distance()?.roundToInt() ?: 4) <= 3
 
         Chat.canContinue()
         if (!killing && script.interactWhileDousing(spirit, "Harpoon", script.side.bossWalkLocation, true)) {
