57ea59809b6d8b1c8c3d34433cff19144f61b5cd
krulvis <git@krulvis.space>
2022-12-14 23:12:34 +0100

    IronMan looting fixed

--- src/main/kotlin/org/powbot/krulvis/api/extensions/watcher/LootWatcher.kt
+++ src/main/kotlin/org/powbot/krulvis/api/extensions/watcher/LootWatcher.kt
@@ -5,10 +5,11 @@ import org.powbot.api.event.TickEvent
 import org.powbot.api.rt4.GroundItem
 import org.powbot.api.rt4.GroundItems
 import org.powbot.api.rt4.Npc
+import org.powbot.krulvis.api.ATContext.debug
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
 
-class LootWatcher(private val tile: Tile, private val radius: Int = 3, private val isLoot: (GroundItem) -> Boolean) :
+class LootWatcher(private val tile: Tile, private val radius: Int = 4, private val isLoot: (GroundItem) -> Boolean) :
     Watcher() {
 
     private val latch = CountDownLatch(1)
@@ -20,8 +21,10 @@ class LootWatcher(private val tile: Tile, private val radius: Int = 3, private v
     @com.google.common.eventbus.Subscribe
     fun onTickEvent(_e: TickEvent) {
         val groundItems = groundItems()
-        if (groundItems != startLoot) {
-            loot.addAll(groundItems.filterNot { it in startLoot }.filter(isLoot))
+        val newLoot = groundItems.filterNot { it in startLoot }
+        if (newLoot.isNotEmpty()) {
+            debug("New loot found: [${newLoot.joinToString { it.name() }}]")
+            loot.addAll(newLoot.filter(isLoot))
             latch.countDown()
         }
     }
