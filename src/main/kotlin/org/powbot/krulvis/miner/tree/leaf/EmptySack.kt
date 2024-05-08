73af54f96d136daaa9f26a1fa7ffe0aef93457c5
Krulvis <joepkleint@gmail.com>
2024-05-08 16:08:58 +0200

    Add escaping top floor to emptying sack

--- src/main/kotlin/org/powbot/krulvis/miner/tree/leaf/EmptySack.kt
+++ src/main/kotlin/org/powbot/krulvis/miner/tree/leaf/EmptySack.kt
@@ -2,6 +2,7 @@ package org.powbot.krulvis.miner.tree.leaf
 
 import org.powbot.api.rt4.Inventory
 import org.powbot.api.script.tree.Leaf
+import org.powbot.krulvis.api.ATContext.canReach
 import org.powbot.krulvis.api.ATContext.walkAndInteract
 import org.powbot.krulvis.api.utils.Utils.mid
 import org.powbot.krulvis.api.utils.Utils.waitFor
@@ -12,7 +13,9 @@ class EmptySack(script: Miner) : Leaf<Miner>(script, "Emptying sack") {
     override fun execute() {
         val sack = script.getSack()
         sack.ifPresent {
-            if (walkAndInteract(it, "Search")) {
+            if (!it.canReach()) {
+                !script.escapeTopFloor()
+            } else if (walkAndInteract(it, "Search")) {
                 waitFor(mid() + it.distance().roundToInt() * 400) { Inventory.isFull() }
             }
         }
