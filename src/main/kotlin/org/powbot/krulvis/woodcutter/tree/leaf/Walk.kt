299de0959ae6113272dbc7a7f963dd45ca88504f
Krulvis <joepkleint@gmail.com>
2024-07-03 12:06:56 +0200

    Woodcutter 1.1.1 support redwood in farming guild

--- src/main/kotlin/org/powbot/krulvis/woodcutter/tree/leaf/Walk.kt
+++ src/main/kotlin/org/powbot/krulvis/woodcutter/tree/leaf/Walk.kt
@@ -1,58 +1,72 @@
 package org.powbot.krulvis.woodcutter.tree.leaf
 
 import org.powbot.api.Tile
+import org.powbot.api.rt4.Bank
+import org.powbot.api.rt4.DepositBox
 import org.powbot.api.rt4.Movement
+import org.powbot.api.rt4.Objects
 import org.powbot.api.script.tree.Leaf
+import org.powbot.krulvis.api.ATContext.distanceM
+import org.powbot.krulvis.api.ATContext.me
+import org.powbot.krulvis.api.ATContext.walkAndInteract
 import org.powbot.krulvis.api.utils.Utils.long
 import org.powbot.krulvis.api.utils.Utils.waitFor
+import org.powbot.krulvis.api.utils.Utils.waitForDistance
 import org.powbot.krulvis.woodcutter.Woodcutter
 import org.powbot.mobile.script.ScriptManager
 
 class Walk(script: Woodcutter) : Leaf<Woodcutter>(script, "Walk to Trees") {
-    override fun execute() {
-        val locs = script.trees
-        script.chopDelay.forceFinish()
-        if (locs.isEmpty()) {
-            script.logger.info("Script requires at least 1 Tree GameObject set in the Configuration")
-            ScriptManager.stop()
-        } else {
-            val tile = locs.minByOrNull { it.distance() }
-            if (script.forceWeb) {
-                Movement.builder(tile).setForceWeb(true).move()
-                return
-            }
-            if (tile != null && tile.distance() < 15) {
-                Movement.step(tile)
-                waitFor(long()) { tile.distance() < 5 }
-            } else {
-                Movement.walkTo(tile?.getStraightNeighor())
-            }
-        }
-    }
+	override fun execute() {
+		val locs = script.trees
+		Bank.close()
+		DepositBox.close()
+		script.chopDelay.forceFinish()
+		if (locs.isEmpty()) {
+			script.logger.info("Script requires at least 1 Tree GameObject set in the Configuration")
+			ScriptManager.stop()
+		} else {
+			val tile = locs.minByOrNull { it.distance() } ?: return
+			if (script.forceWeb) {
+				Movement.builder(tile).setForceWeb(true).move()
+				return
+			}
+			if (tile.distance() < 15) {
+				Movement.step(tile)
+				waitFor(long()) { tile.distance() < 5 }
+			} else if (tile.floor == 1 && tile.distanceM(me) <= 20 && me.floor() == 0) {
+				val ladder = Objects.stream().name("Rope ladder").action("Climb-up").nearest().first()
+				if (walkAndInteract(ladder, "Climb-up")) {
+					waitForDistance(ladder) { me.floor() == 1 }
+				}
+			} else {
+				Movement.walkTo(tile.getStraightNeighor())
+			}
+		}
+	}
 
-    /**
-     * Returns: [Tile] nearest neighbor or self as which is walkable
-     */
-    fun Tile.getStraightNeighor(
-    ): Tile? {
-        return getStraightNeighors().minByOrNull { it.distance() }
-    }
+	/**
+	 * Returns: [Tile] nearest neighbor or self as which is walkable
+	 */
+	fun Tile.getStraightNeighor(
+	): Tile? {
+		return getStraightNeighors().minByOrNull { it.distance() }
+	}
 
-    fun Tile.getStraightNeighors(
-    ): List<Tile> {
+	fun Tile.getStraightNeighors(
+	): List<Tile> {
 
-        val t = tile()
-        val x = t.x()
-        val y = t.y()
-        val f = t.floor()
-        val cm = Movement.collisionMap(f).flags()
+		val t = tile()
+		val x = t.x()
+		val y = t.y()
+		val f = t.floor()
+		val cm = Movement.collisionMap(f).flags()
 
-        val neighbors = mutableListOf<Tile>()
-        for (yy in 1..2) neighbors.add(Tile(x, y + yy, f))
-        for (yy in 1..2) neighbors.add(Tile(x, y - yy, f))
-        for (xx in 1..2) neighbors.add(Tile(x + xx, y, f))
-        for (xx in 1..2) neighbors.add(Tile(x - xx, y, f))
+		val neighbors = mutableListOf<Tile>()
+		for (yy in 1..2) neighbors.add(Tile(x, y + yy, f))
+		for (yy in 1..2) neighbors.add(Tile(x, y - yy, f))
+		for (xx in 1..2) neighbors.add(Tile(x + xx, y, f))
+		for (xx in 1..2) neighbors.add(Tile(x - xx, y, f))
 
-        return neighbors.filter { !it.blocked(cm) }
-    }
+		return neighbors.filter { !it.blocked(cm) }
+	}
 }
