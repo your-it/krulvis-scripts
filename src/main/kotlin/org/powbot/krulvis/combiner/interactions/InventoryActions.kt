527585716fab346c6cd7f1e6d52254e3dfdc29d8
--local <joepkleint@gmail.com>
2024-07-09 20:10:20 +0200

    Errors for compiler

--- src/main/kotlin/org/powbot/krulvis/combiner/interactions/InventoryActions.kt
+++ src/main/kotlin/org/powbot/krulvis/combiner/interactions/InventoryActions.kt
@@ -9,21 +9,21 @@ import kotlin.math.abs
 
 object InventoryActions {
 
-	/**
-	 * @return the first item matching a name or the item closest to the "using" item
-	 */
-	fun InventoryItemActionEvent.getItem(): Item? {
-		val itemName = if (name.contains("->")) name.substring(name.indexOf("-> ") + 3) else name
-		val item = if (isUsingItem()) {
-			val prevItemIndex = Inventory.stream().name(itemUsing()).first().inventoryIndex
-			val allRelevantItemsSorted = Inventory.stream().name(itemName).toList()
-				.map { abs(it.inventoryIndex - prevItemIndex) to it }.sortedBy { it.first }
-			val distance = allRelevantItemsSorted.firstOrNull()?.first ?: return null
-			allRelevantItemsSorted.filter { it.first == distance }.maxBy { it.second.inventoryIndex }.second
-		} else
-			Inventory.stream().name(itemName).firstOrNull()
-		return item
-	}
+    /**
+     * @return the first item matching a name or the item closest to the "using" item
+     */
+    fun InventoryItemActionEvent.getItem(): Item? {
+        val itemName = if (name.contains("->")) name.substring(name.indexOf("-> ") + 3) else name
+        val item = if (isUsingItem()) {
+            val prevItemIndex = Inventory.stream().name(itemUsing()).first().inventoryIndex
+            val allRelevantItemsSorted = Inventory.stream().name(itemName).toList()
+                .map { abs(it.inventoryIndex - prevItemIndex) to it }.sortedBy { it.first }
+            val distance = allRelevantItemsSorted.firstOrNull()?.first ?: return null
+            allRelevantItemsSorted.filter { it.first == distance }.maxByOrNull { it.second.inventoryIndex }?.second
+        } else
+            Inventory.stream().name(itemName).firstOrNull()
+        return item
+    }
 
 }
 
